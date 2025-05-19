3093fa467cf5b6b2b8172f6853cc642688e49938
agustincalcagni <agustincalcagni@gmail.com>
2025-05-19 20:46:47 -0300

    fix .env

--- src/app/supabase/client.ts
+++ src/app/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const ANNON_KEY = process.env.NEXT_PUBLIC_ANNON_KEY!
+const ANNON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(URL, ANNON_KEY)
