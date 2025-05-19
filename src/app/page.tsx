08adc248f49fa04d45be41654922f304015edc09
agustincalcagni <agustincalcagni@gmail.com>
2025-05-19 20:54:25 -0300

    fix nullish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import { allProfiles } from '../../.contentlayer/generated'
+import { allProfiles } from "../../.contentlayer/generated";
 import { notFound } from "next/navigation";
 
 export default function Home() {
-  const profile = allProfiles.find((item) => item._raw.flattenedPath === "profile/principal")
+  const profile = allProfiles.find(
+    (item) => item._raw.flattenedPath === "profile/principal"
+  );
   if (!profile) {
-    notFound()
+    notFound();
   }
- 
+
   return (
     <section>
       <header className="flex justify-between bg-[#F5F4F2] dark:bg-zinc-950/20 md:max-h-[240px] p-10 relative border-b border-zinc-200/70 dark:border-zinc-700/30 items-center overflow-hidden">
@@ -36,20 +38,24 @@ export default function Home() {
       </header>
 
       <div className="p-6 space-y-4 dark:text-zinc-200">
-         <p>{profile?.body.raw}</p>
+        <p>{profile?.body.raw}</p>
       </div>
 
-      
       <div className="overflow-hidden whitespace-nowrap py-4 bg-zinc-100 dark:bg-zinc-900">
         <div className="inline-block whitespace-nowrap font-semibold text-lg text-zinc-700 dark:text-zinc-300 animate-[marquee_20s_linear_infinite]">
-         {allProfiles.map((item) => {
-          const skills = item.skills.split(",") || "No, Disponible"
-          return (
-            <span key={item._id}>
-            {skills.map((skill) => skill.concat(" • ")) ?? ""}
-            </span>
-          )
-         })}
+          {allProfiles.map((item) => {
+            const skills =
+              typeof item.skills === "string"
+                ? item.skills.split(",")
+                : ["No disponible"];
+            return (
+              <span key={item._id}>
+                {skills.map((skill, index) => (
+                  <span key={index}>{skill.trim()} • </span>
+                ))}
+              </span>
+            );
+          })}
         </div>
       </div>
     </section>
