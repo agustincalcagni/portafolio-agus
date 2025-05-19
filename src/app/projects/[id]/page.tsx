4863ca995e1ae7e03ab585f92b8e39f8c361b723
agustincalcagni <agustincalcagni@gmail.com>
2025-05-19 20:31:00 -0300

    fixies

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,37 +1,39 @@
-import Link from 'next/link';
 import { allProjects } from '../../../../.contentlayer/generated';
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
 import { dateFormatter } from '@/app/consts';
-import { MoveLeft } from 'lucide-react'
+import { MoveLeft } from 'lucide-react';
 import MarkdownRenderer from '@/app/components/MarkDownrenderer';
 
-type ProjectPageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function ProjectPage({ params }: ProjectPageProps) {
+export default async function ProjectPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const id =  params.id
   const project = allProjects.find(
-    (proj) => proj._raw.flattenedPath === `projects/${params.id}`
+    (proj) => proj._raw.flattenedPath === `projects/${id}`
   );
 
-  if (!project) {
-    notFound();
-  }
+  if (!project) notFound();
 
   return (
     <section className="p-6 dark:text-zinc-200">
-        <span>
-        <Link href="/projects" className='flex gap-1 items-center mb-3 hover:underline'>
-        <MoveLeft />
-         Volver
+      <span>
+        <Link
+          href="/projects"
+          className="flex gap-1 items-center mb-3 hover:underline"
+        >
+          <MoveLeft />
+          Volver
         </Link>
-        </span>
+      </span>
       <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
-      <p className="text-gray-600 mb-4">{dateFormatter({ lang: "es-AR", rawDate: project.date })}</p>
+      <p className="text-gray-600 mb-4">
+        {dateFormatter({ lang: 'es-AR', rawDate: project.date })}
+      </p>
       <div className="prose">
-        <MarkdownRenderer content={project.body.raw} />
+        <MarkdownRenderer content={project.body.raw ?? ''} />
       </div>
     </section>
   );
