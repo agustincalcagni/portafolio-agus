96213072e93e61da6a5802caa33c567bfa8bf4a9
agustincalcagni <agustincalcagni@gmail.com>
2025-05-20 03:57:07 -0300

    changed name title

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import { HeaderStyle } from "../components/HeaderStyle";
 export default function ContactPage() {
   return (
     <section className="dark:text-zinc-200">
-      <HeaderStyle title="Formulario de Contacto" />
+      <HeaderStyle title="Contacto" />
       <div className="p-6">
         <ContactForm />
       </div>
