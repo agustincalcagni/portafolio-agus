d99e6ad5bb0d8aeacc6488a6001cc4fbe82a9668
agustincalcagni <agustincalcagni@gmail.com>
2025-05-20 03:00:36 -0300

    feat: create new Marquee Comp. and mod styles

---
+++ src/app/components/MarqueeLogos.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+
+export const MarqueeLogos = () => {
+  return (
+    <div className="overflow-hidden whitespace-nowrap py-4">
+      <div className="inline-flex gap-10 whitespace-nowrap font-semibold text-lg text-zinc-600 dark:text-[#626262] animate-[marquee_26s_linear_infinite] text-shadow cursor-default">
+        <Image
+          src={"/Google_Cloud_logo.svg"}
+          width={160}
+          height={90}
+          alt=""
+          className="marquee-images"
+          fetchPriority="high"
+        />
+        <Image
+          src={"/logo-qlik_d49uek.svg"}
+          width={60}
+          height={60}
+          alt=""
+          className="marquee-images"
+          fetchPriority="high"
+        />
+        <Image
+          src={"/Snowflake_Logo.svg"}
+          width={120}
+          height={45}
+          alt=""
+          className="marquee-images"
+          fetchPriority="high"
+        />
+        <span className="flex gap-2 items-center font-['system-ui']">
+          Microsoft Power BI
+          <Image
+            src={"/New_Power_BI_Logo.svg"}
+            width={45}
+            height={45}
+            alt=""
+            className="marquee-images"
+            fetchPriority="high"
+          />
+        </span>
+        <span className="flex gap-2 items-center font-['system-ui']">
+          Microsoft Excel
+          <Image
+            src={"/Microsoft_Office_Excel_(2019–present).svg"}
+            width={45}
+            height={45}
+            alt=""
+            className="marquee-images"
+            fetchPriority="high"
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { allProfiles } from "../../.contentlayer/generated";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { Mail, Linkedin } from "lucide-react";
+import { MarqueeLogos } from "./components/MarqueeLogos";
 
 export default function Home() {
   const profile = allProfiles.find(
@@ -44,62 +45,14 @@ export default function Home() {
         <p>{profile?.body.raw}</p>
       </div>
 
-      <div className="overflow-hidden whitespace-nowrap py-4">
-        <div className="inline-flex gap-10 whitespace-nowrap font-semibold text-lg text-zinc-600 dark:text-[#626262] animate-[marquee_20s_linear_infinite] text-shadow">
-          <Image
-            src={"/Google_Cloud_logo.svg"}
-            width={160}
-            height={90}
-            alt=""
-            className="marquee-images"
-            fetchPriority="high"
-          />
-          <Image
-            src={"/logo-qlik_d49uek.svg"}
-            width={60}
-            height={60}
-            alt=""
-            className="marquee-images"
-            fetchPriority="high"
-          />
-          <Image
-          src={"/Snowflake_Logo.svg"}
-          width={120}
-          height={45}
-          alt=""
-          className="marquee-images"
-          fetchPriority="high"
-          />
-          <span className="flex gap-2 items-center font-['system-ui']">
-            Microsoft Power BI
-            <Image
-              src={"/New_Power_BI_Logo.svg"}
-              width={45}
-              height={45}
-              alt=""
-              className="marquee-images"
-              fetchPriority="high"
-            />
-          </span>
-          <span className="flex gap-2 items-center font-['system-ui']">
-            Microsoft Excel
-            <Image
-              src={"/Microsoft_Office_Excel_(2019–present).svg"}
-              width={45}
-              height={45}
-              alt=""
-              className="marquee-images"
-              fetchPriority="high"
-            />
-          </span>
-        </div>
-      </div>
-      <footer className="flex justify-center dark:text-zinc-200 mt-4 gap-3">
-        <Link href="mailto:agustincalcagni@gmail.com" className="flex items-center gap-2 hover:text-[#FF7759]">
+      <MarqueeLogos />
+
+      <footer className="flex justify-between text-zinc-800 dark:text-zinc-200 mt-4 gap-3 px-6">
+        <Link href="mailto:agustincalcagni@gmail.com" className="flex items-center gap-2 hover:text-[#FF7759] transition-colors duration-200">
           <Mail />
           agustincalcagni@gmail.com
         </Link>
-        <Link href="https://www.linkedin.com/in/agustincalcagni/" className="flex items-center gap-2 hover:text-[#FF7759]">
+        <Link href="https://www.linkedin.com/in/agustincalcagni/" className="flex items-center gap-2 hover:text-[#FF7759] transition-colors duration-200">
           <Linkedin />
           agustincalcagni
         </Link>
