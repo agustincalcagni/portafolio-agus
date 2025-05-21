79e46a993ae5351526e5f809bd9146cf894c020b
agustincalcagni <agustincalcagni@gmail.com>
2025-05-20 23:21:14 -0300

    feat: updated svg icons

--- src/app/components/MarqueeLogos.tsx
+++ src/app/components/MarqueeLogos.tsx
@@ -4,11 +4,21 @@ const Logos = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap py-4">
       <div className="inline-flex gap-10 whitespace-nowrap font-semibold text-lg text-zinc-600 dark:text-[#626262] text-shadow cursor-default">
+        <span className="flex gap-2 items-center font-['system-ui']">
+          <Image
+            src={"/PostgreSQL_logo.svg"}
+            width={45}
+            height={45}
+            color="#fafafa"
+            alt="PostgreSQL logo"
+          />
+          PostgreeSQL
+        </span>
         <Image
           src={"/Google_Cloud_logo.svg"}
           width={160}
           height={90}
-          alt=""
+          alt="Google Cloud logo"
           className="marquee-images"
           fetchPriority="high"
         />
@@ -16,7 +26,7 @@ const Logos = () => {
           src={"/logo-qlik_d49uek.svg"}
           width={60}
           height={60}
-          alt=""
+          alt="Qlik logo"
           className="marquee-images"
           fetchPriority="high"
         />
@@ -24,31 +34,31 @@ const Logos = () => {
           src={"/Snowflake_Logo.svg"}
           width={120}
           height={45}
-          alt=""
+          alt="SnowFlake logo"
           className="marquee-images"
           fetchPriority="high"
         />
         <span className="flex gap-2 items-center font-['system-ui']">
-          Power BI
           <Image
             src={"/New_Power_BI_Logo.svg"}
             width={40}
             height={40}
-            alt=""
+            alt="Microsoft Power BI logo"
             className="marquee-images"
             fetchPriority="high"
           />
+          Power BI
         </span>
         <span className="flex gap-2 items-center font-['system-ui']">
-          Excel
           <Image
             src={"/Microsoft_Office_Excel_(2019–present).svg"}
             width={45}
             height={45}
-            alt=""
+            alt="Microsoft Excel logo"
             className="marquee-images"
             fetchPriority="high"
           />
+          Excel
         </span>
       </div>
     </div>
